24816ccf516b97e9ecd53ecd83926bb2dc764203
kolynzb <collinsbenda360@gmail.com>
2022-06-30 21:49:59 +0300

    Add font

--- src/ts/Bruno.ts
+++ src/ts/Bruno.ts
@@ -1,1 +1,11 @@
 import "../styles/bruno.css";
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+const aspectRatio: number = window.innerWidth / window.innerHeight;
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
+const bgCanvas: Element = document.querySelector("#bruno")!;
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: bgCanvas,
+});
